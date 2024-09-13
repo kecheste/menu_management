88dd750ec561365fc5de8c5056ce284ab3019812
Tadiwos Anegagregn <tadiwosanegagregn@gmail.com>
2024-09-13 20:55:23 +0300

    vercel

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://menu-management-ltyh.vercel.app/',
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
