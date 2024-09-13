7dc350867abd2d97120d282854b16184f5201dba
Abenezer <aben.ezer443@gmail.com>
2024-09-13 18:31:35 +0300

    UI Completed

---
+++ client/src/app/components/Sidebar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { MdMenuOpen } from "react-icons/md";
+import { FaFolder } from "react-icons/fa";
+import { CiGrid41 } from "react-icons/ci";
+import { CiFolderOn } from "react-icons/ci";
+
+const Sidebar = ({
+  activeMenu,
+  setActiveMenu,
+}: {
+  activeMenu: string;
+  setActiveMenu: (menu: string) => void;
+}) => {
+  return (
+    <aside className="w-64 px-4 pt-4 bg-gray-900 text-white h-full rounded-3xl">
+      <div className="flex items-center justify-between">
+        <div className="p-4 font-bold text-lg">CLOIT</div>
+        <MdMenuOpen size={25} color="white" className="cursor-pointer" />
+      </div>
+      <ul className="mt-8 space-y-2 bg-gray-800 rounded-2xl py-4">
+        <li
+          className={`px-4 py-2 cursor-pointer flex items-center gap-4 ${
+            activeMenu === "Systems" ? "bg-green-500 rounded-lg px-4 mx-1" : ""
+          }`}
+          onClick={() => setActiveMenu("Systems")}
+        >
+          <FaFolder size={22} color="white" />
+          <p className="text-grey-100">Systems</p>
+        </li>
+        <li
+          className={`px-4 py-2 cursor-pointer flex items-center gap-4 ${
+            activeMenu === "Menus" ? "bg-green-500 rounded-lg px-4 mx-1" : ""
+          }`}
+          onClick={() => setActiveMenu("Menus")}
+        >
+          <CiGrid41 size={22} className="text-gray-100" />
+          <p className="text-grey-100">Menus</p>
+        </li>
+        <li
+          className={`px-4 py-2 cursor-pointer flex items-center gap-4 ${
+            activeMenu === "API List" ? "bg-green-500 rounded-lg px-4 mx-1" : ""
+          }`}
+          onClick={() => setActiveMenu("API List")}
+        >
+          <CiGrid41 size={22} className="text-gray-100" />
+          <p className="text-grey-100">API List</p>
+        </li>
+        <li
+          className={`px-4 py-2 cursor-pointer flex items-center gap-4 ${
+            activeMenu === "SystemCode"
+              ? "bg-green-500 rounded-lg px-4 mx-1"
+              : ""
+          }`}
+          onClick={() => setActiveMenu("SystemCode")}
+        >
+          <CiGrid41 size={22} className="text-gray-100" />
+          <p className="text-grey-100">SystemCode</p>
+        </li>
+      </ul>
+      <ul className="mt-6 space-y-2">
+        <li
+          className={`px-4 py-2 cursor-pointer flex items-center gap-4 ${
+            activeMenu === "Users & Group"
+              ? "bg-green-500 rounded-lg px-4 mx-1"
+              : ""
+          }`}
+          onClick={() => setActiveMenu("Users & Group")}
+        >
+          <CiFolderOn size={22} className="text-gray-300" />
+          <p className="text-grey-100">Users & Group</p>
+        </li>
+        <li
+          className={`px-4 py-2 cursor-pointer flex items-center gap-4 ${
+            activeMenu === "Competition"
+              ? "bg-green-500 rounded-lg px-4 mx-1"
+              : ""
+          }`}
+          onClick={() => setActiveMenu("Competition")}
+        >
+          <CiFolderOn size={22} className="text-gray-300" />
+          <p className="text-grey-100">Competition</p>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,101 +1,74 @@
-import Image from "next/image";
+"use client";
+
+import Sidebar from "./components/Sidebar";
+import { BsGridFill } from "react-icons/bs";
+import { FaFolder } from "react-icons/fa";
+import { TreeView } from "@primer/react";
+import Button from "./components/Button";
+import InputForm from "./components/InputForm";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [activeMenu, setActiveMenu] = useState("Menus");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  return (
+    <div className="flex h-screen p-8">
+      <Sidebar activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+      <main className="grid grid-cols-12 gap-20 px-10">
+        <div className="col-span-6 flex-col">
+          <div className="flex items-center gap-3 mb-10">
+            <FaFolder size={22} className="text-gray-300" />
+            <p className="text-gray-400">/</p>
+            <p className="text-gray-600">{activeMenu}</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <p className="bg-blue-600 p-3 rounded-full">
+              <BsGridFill size={22} color="white" />
+            </p>
+            <h1 className="text-3xl font-bold text-black">{activeMenu}</h1>
+          </div>
+          <div className="my-4 flex items-center">
+            <select className="border rounded-md bg-gray-100 rounded-xl border-none text-gray-600 px-6 py-2 outline-none">
+              <option>System management</option>
+            </select>
+          </div>
+          <div className="flex items-center gap-4 mb-6">
+            <button className="bg-gray-900 text-white px-8 py-1.5 rounded-full">
+              Expand All
+            </button>
+            <button className="text-gray-900 border px-8 py-1.5 rounded-full">
+              Collapse All
+            </button>
+          </div>
+
+          <TreeView className="text-gray-800" aria-label="Files changed">
+            <TreeView.Item id="src" defaultExpanded>
+              src
+              <TreeView.SubTree>
+                <TreeView.Item id="src/Avatar.tsx">Avatar.tsx</TreeView.Item>
+                <TreeView.Item id="src/Button.tsx" current>
+                  Button.tsx
+                </TreeView.Item>
+              </TreeView.SubTree>
+            </TreeView.Item>
+            <TreeView.Item id="package.json">package.json</TreeView.Item>
+          </TreeView>
+        </div>
+        <div className="col-span-6 flex flex-col h-full justify-center">
+          <ul className="flex flex-col gap-3">
+            <InputForm
+              text="Menu ID"
+              type="text"
+              placeholder="5ghjhf-38fhj3h-38fjkb"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <InputForm text="Depth" type="number" placeholder="3" />
+            <InputForm text="ParentData" type="text" placeholder="Systems" />
+            <InputForm text="Name" type="text" placeholder="System Code" />
+
+            <Button text="Save" />
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
