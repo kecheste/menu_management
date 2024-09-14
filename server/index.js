92b1ec2780c9bdd0efdf40305f8e08855339449a
Abenezer <aben.ezer443@gmail.com>
2024-09-14 05:17:13 +0300

    cors error fix

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://menu-management-ltyh.vercel.app/",
+    origin: "https://menu-management-ltyh.vercel.app",
     // origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
     credentials: true,
@@ -16,10 +16,34 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://menu-management-ltyh.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, PATCH, DELETE, POST, PUT"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
+
 import menuRoutes from "./routes/menu.js";
 import AppError from "./utils/AppError.js";
 
 app.use("/menu", menuRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.all("*", (req, res, next) => {
   next(new AppError("Page Not Found!", 404));
