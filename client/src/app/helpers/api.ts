7e1cd80f35e7c8a397e01a99d73d2da8af04e4aa
Abenezer <aben.ezer443@gmail.com>
2024-09-14 14:39:52 +0300

    added favicon.ico

--- client/src/app/helpers/api.ts
+++ client/src/app/helpers/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://menu-management-api-voxp.onrender.com",
-  // baseURL: "http://localhost:5000",
+  // baseURL: "https://menu-management-api-voxp.onrender.com",
+  baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
