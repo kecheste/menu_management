6900eede9b05cc5fed9da8742845d156a531a87f
Abenezer <aben.ezer443@gmail.com>
2024-09-14 11:06:32 +0300

    finalizing

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -3,7 +3,6 @@
 import { MdMenuOpen } from "react-icons/md";
 import { FaFolder } from "react-icons/fa";
 import { CiGrid41 } from "react-icons/ci";
-import { CiFolderOn } from "react-icons/ci";
 import { useEffect, useState } from "react";
 import api from "../helpers/api";
 
@@ -37,38 +36,43 @@ const Sidebar = ({
     fetchMenus();
   }, []);
 
-  // Function to render the correct icon based on depth
   const renderIcon = (depth: number) => {
     if (depth === 1) {
-      return <CiGrid41 size={22} className="text-gray-100" />
+      return <CiGrid41 size={22} className="text-gray-100" />;
     } else if (depth === 2) {
-      return <FaFolder size={22} color="white" />;;
+      return <FaFolder size={22} color="white" />;
     }
-    return null; // Do not show an icon for depth 3 or more
+    return null;
   };
 
   return (
     <aside className="w-64 px-4 pt-4 md:bg-gray-900 text-white h-full min-w-fit rounded-3xl">
       <div className="flex items-center justify-between">
         <div className="p-4 font-bold text-lg hidden md:block">CLOIT</div>
-        <MdMenuOpen size={25} className="cursor-pointer text-gray-400 md:text-white" />
+        <MdMenuOpen
+          size={25}
+          className="cursor-pointer text-gray-400 md:text-white"
+        />
       </div>
 
       <ul className="mt-8 space-y-2 bg-gray-800 rounded-2xl py-4 hidden md:block min-w-fit">
-        {menus.map((menu) => (
-          menu.Depth <= 2 && ( // Only show items with depth 1 or 2
-            <li
-              key={menu.id}
-              className={`px-4 py-2 cursor-pointer flex items-center justify-start gap-4 ${
-                activeMenu === menu.Name ? "bg-green-500 rounded-lg px-4 mx-1" : ""
-              }`}
-              onClick={() => setActiveMenu(menu.Name)}
-            >
-              {renderIcon(menu.Depth)}
-              <p className="text-grey-100 w-full ">{menu.Name}</p>
-            </li>
-          )
-        ))}
+        {menus.map(
+          (menu) =>
+            menu.Depth <= 2 && (
+              <li
+                key={menu.id}
+                className={`px-4 py-2 cursor-pointer flex items-center justify-start gap-4 ${
+                  activeMenu === menu.Name
+                    ? "bg-green-500 rounded-lg px-4 mx-1"
+                    : ""
+                }`}
+                onClick={() => setActiveMenu(menu.Name)}
+              >
+                {renderIcon(menu.Depth)}
+                <p className="text-grey-100 w-full ">{menu.Name}</p>
+              </li>
+            )
+        )}
       </ul>
     </aside>
   );
--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "https://menu-management-ltyh.vercel.app"
+    // "http://localhost:3000"
   );
   res.header(
     "Access-Control-Allow-Methods",
